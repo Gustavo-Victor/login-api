61032c399b0290572fcc2373a3679cc111e16c2b
Gustavo Victor <gguvictor909@gmail.com>
2023-10-30 14:05:52 -0300

    update login functionality

--- index.js
+++ index.js
@@ -67,13 +67,90 @@ app.post("/auth/register", async(req, res) => {
     }); 
     try {
         const result = await userObj.save(); 
-        res.status(201).json({ message: `User created successfully!` });
+        res.status(201).json({ message: `User registered successfully!` });
     } catch(error) {
         console.log(error); 
-        res.status(500).json({message: `Failed to create user`}); 
+        res.status(500).json({message: `User registration failed`}); 
     }
 }); 
 
+//login user
+app.post("/auth/login", async(req, res) => {
+
+    const { email, password } = req.body; 
+
+    //validation
+    if(!email) {
+        res.status(422).json({message: "Email is required"}); 
+        return ;
+    }
+
+    if(!password) {
+        res.status(422).json({message: "Password is required"}); 
+        return ;
+    }
+
+    //check if user exists
+    const user = await User.findOne({email}); 
+    if(!user) {
+        res.status(404).json({message: "User not found"}); 
+        return ;
+    }
+
+    //check if passwords match
+    const passwordsMatch = await bcrypt.compare(password, user.password); 
+    if(!passwordsMatch) {
+        res.status(422).json({message: "Invalid password"}); 
+        return ; 
+    }
+
+    //login user
+    const secret = process.env.SECRET; 
+    
+    try {
+        const token = jwt.sign(
+            { id: user._id, }, 
+            secret
+        ); 
+        res.status(200).json({ message: "User successfully authenticated", token}); 
+    } catch (error) {
+        console.log(error); 
+        res.status(500).json({message: "User authentication failed"}); 
+    }
+}); 
+
+//private route
+app.get("/user/:id", checkToken, async(req, res) => {
+    const { id } = req.params; 
+
+    //check if user exists
+    try {
+        const user = await User.findById(id, "-password"); 
+        res.status(200).json(user); 
+    } catch (error) {
+        console.log(error); 
+        res.status(404).json({message: "User not found"}); 
+    }
+});
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers["authorization"]; 
+    const token = authHeader && authHeader.split(" ")[1]; 
+
+    if(!token) {
+        res.status(401).json({message: "Access denied"});
+        return ;  
+    }
+
+    try {
+        const secret = process.env.SECRET; 
+        jwt.verify(token, secret); 
+        next(); 
+    } catch (error) {
+        console.log(error); 
+        res.status(400).json({message: "Invalid token"}); 
+    }
+}
 
 //database connection
 mongoose.connect(`${DB_URI}`)
