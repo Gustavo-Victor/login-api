55d1daef9b9cc6337bf716992b714da407930e9e
Gustavo Victor <gguvictor909@gmail.com>
2023-10-30 10:02:18 -0300

    feat: update database connection

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 import express from "express"; 
 import bycript from "bcrypt"; 
 import jwt from "jsonwebtoken"; 
-import "./db.js"; 
+import mongoose from "mongoose";
+import { config } from "dotenv"; 
+// import "./db.js"; 
 
+config(); 
 const app = express(); 
-const { PORT } = process.env;
+const { PORT, DB_URI } = process.env;
 
 //public route
 app.get("/", (req, res) => {
     res.status(200).json({message: "Welcome to our API!"}); 
 }); 
 
-const listener = app.listen(PORT || 3000, () => {
-    console.log(`App is running on http://localhost:${PORT}`); 
-}); 
+
+mongoose.connect(`${DB_URI}`)
+    .then(resp => {
+        const listener = app.listen(PORT || 3000, () => {
+            console.log(`App is running on http://localhost:${PORT}`); 
+            console.log(`Connected successfully!`);
+            // console.log(resp); 
+        }); 
+    })
+    .catch(err => {
+        console.log(`Connection failed: ${err}`); 
+    })
+
 
 
